40aaf7e848a81284a05ef894be122afa5a711194
Ahmad <ahmadrameen.alizai@gmail.com>
2023-01-31 19:12:26 +0500

    Lesson 66 - Tkinter and API added

--- app.py
+++ app.py
@@ -1,71 +1,29 @@
-# Creating address book using Tkinter
-from tkinter import *
+import requests 
+# python -m pip install pip
+# python3 -m pip install pip
+# pip install requests
+# pip3 install requests
 
-root = Tk()
+import tkinter as tk
+from tkinter import END, Text
+from tkinter import Button
 
-root.geometry('400x500')
+#API = https://api.quotable.io/random
 
-datas = []
+root = tk.Tk()
+root.title('Quotes')
 
-def add():
-    global datas
-    datas.append([Name.get(),Number.get(),address.get(1.0, "end-1c")])
-    update_book()
+def get_quote():
+    r = requests.get('https://api.quotable.io/random')
+    data = r.json()
+    quote = data['content']
 
-def view():
-    Name.set(datas[int(select.curselection()[0])][0])
-    Number.set(datas[int(select.curselection()[0])][1])
-    address.delete(1.0,"end")
-    address.insert(1.0, datas[int(select.curselection()[0])][2])
+    text_box.delete('1.0', END)
+    text_box.insert(END, quote)
 
-def delete():
-    del datas[int(select.curselection()[0])]
-    update_book()
+text_box = Text(root, height=10, width=50)
+get_button = Button(root, text='Get Quote', command=get_quote)
 
-def reset():
-    Name.set('')
-    Number.set('')
-    address.delete(1.0, "end")
-
-def update_book():
-    select.delete(0, END)
-    for n,p,a in datas:
-        select.insert(END, n)
-
-Name = StringVar()
-Number = StringVar()
-
-# Frames
-frame = Frame()
-frame.pack(pady=10)
-
-frame1 = Frame()
-frame1.pack()
-
-frame2 = Frame()
-frame2.pack(pady=10)
-
-# Adding elements into frames
-Label(frame, text='Name', font="arial 12 bold").pack(side=LEFT)
-Entry(frame, textvariable = Name, width=50).pack()
-
-Label(frame1, text='Phone No', font="arial 12 bold").pack(side=LEFT)
-Entry(frame1, textvariable = Number, width=50).pack()
-
-Label(frame2, text='Address', font="arial 12 bold").pack(side=LEFT)
-address = Text(frame2, width=37, height=10)
-address.pack()
-
-# Adding buttons for functions
-Button(root, text="Add", font="arial 12 bold",command=add).place(x=100, y=270)
-Button(root, text="View", font="arial 12 bold",command=view).place(x=100, y=310)
-Button(root, text="Delete", font="arial 12 bold",command=delete).place(x=100, y=350)
-Button(root, text="Reset", font="arial 12 bold",command=reset).place(x=100, y=390)
-
-scroll_bar = Scrollbar(root, orient=VERTICAL)
-select = Listbox(root, yscrollcommand=scroll_bar.set, height=12)
-scroll_bar.config(command=select.yview)
-scroll_bar.pack(side=RIGHT, fill=Y)
-select.place(x=200, y=260)
-
+text_box.pack()
+get_button.pack()
 root.mainloop()
